5a93559baae5e237427d78bd61602906d3c02606
root <root@ip-172-31-41-254.ap-south-1.compute.internal>
2024-08-13 03:46:04 +0000

    This is jenkins installation script

--- jenkins.sh
+++ jenkins.sh
@@ -1,1 +1,15 @@
+apt update -y && apt upgrade -y && apt install openjdk-17-jdk -y
+java --version
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update -y 
+sudo apt install jenkins -y
+systemctl start jenkins
+
+cat /var/lib/jenkins/secrets/initialAdminPassword
+
 
